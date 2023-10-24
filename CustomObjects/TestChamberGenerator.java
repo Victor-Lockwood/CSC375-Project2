125a6936a3014292527c3db57e8a11392cf06634
Victor-Lockwood <vlockwoo@oswego.edu>
2023-10-24 13:25:47 -0400

    Restructuring

--- CustomObjects/TestChamberGenerator.java
+++
@@ -1,6 +0,0 @@
-package CustomObjects;
-
-public class TestChamberGenerator {
-    String[] FIRST_NAMES = {"BILLY", "BOB", "CAROLINE", "CAVE", "FACT", "SPACE", "WHEATLEY", "CHELL", "KEVIN", "RICK", "VICTOR"};
-    String[] LAST_NAMES = {"CORE", "SPHERE", "JOHNSON", "LOCKWOOD", "ECHO", ""};
-}
